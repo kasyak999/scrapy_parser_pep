f0d35c811d67d83e9ab528067d3df06649f5c9fb
Максим <kasyak999@yandex.ru>
2025-04-19 01:35:06 +0300

    up

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -4,16 +4,23 @@ import datetime
 
 
 class PepParsePipeline:
-    filename = (
-        'results/status_summary_'
-        f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
-    )
-    file = open(filename, mode='w', encoding='utf-8')
-    total = 0
-    status_dict = defaultdict(int)
-
     def open_spider(self, spider):
-        self.file.write('Статус,Количество\n')
+        self.total = 0
+        self.status_dict = defaultdict(int)
+        self.filename = (
+            'results/status_summary_'
+            f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+        )
+        with open(self.filename, mode='w', encoding='utf-8') as f:
+            f.write('Статус,Количество\n')
+
+    # def open_spider(self, spider):
+    #     filename = (
+    #         'results/status_summary_'
+    #         f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+    #     )
+    #     self.file = open(filename, mode='w', encoding='utf-8')
+    #     self.file.write('Статус,Количество\n')
 
     def process_item(self, item, spider):
         self.total += 1
@@ -22,7 +29,12 @@ class PepParsePipeline:
         return item
 
     def close_spider(self, spider):
-        for status, count in self.status_dict.items():
-            self.file.write(f'{status},{count}\n')
-        self.file.write(f"Total,{self.total}\n")
-        self.file.close()
+        # for status, count in self.status_dict.items():
+        #     self.file.write(f'{status},{count}\n')
+        # self.file.write(f"Total,{self.total}\n")
+        # self.file.close()
+
+        with open(self.filename, mode='a', encoding='utf-8') as f:
+            for status, count in self.status_dict.items():
+                f.write(f'{status},{count}\n')
+            f.write(f"Total,{self.total}\n")
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,3 +1,4 @@
+# import re
 import scrapy
 from pep_parse.items import PepParseItem
 
@@ -5,16 +6,46 @@ from pep_parse.items import PepParseItem
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
+
+    # def parse(self, response):
+    #     all_pep = response.css(
+    #         'table.pep-zero-table.docutils.align-default tbody tr')
+    #     for pep in all_pep:
+    #         name_and_number = pep.css('a::text').getall()
+    #         data = {
+    #             'number': name_and_number[0].strip(),
+    #             'name': name_and_number[1].strip(),
+    #             'status': pep.css('abbr::text').get()
+    #         }
+    #         yield PepParseItem(data)
 
     def parse(self, response):
         all_pep = response.css(
             'table.pep-zero-table.docutils.align-default tbody tr')
         for pep in all_pep:
+            pep_url = pep.css('a::attr(href)').get()
             name_and_number = pep.css('a::text').getall()
             data = {
                 'number': name_and_number[0].strip(),
                 'name': name_and_number[1].strip(),
-                'status': pep.css('abbr::text').get()
             }
-            yield PepParseItem(data)
+            if pep_url is not None:
+                yield response.follow(
+                    pep_url, callback=self.parse_pep, meta={'data': data})
+
+    def parse_pep(self, response):
+        # pep_info = response.css('h1.page-title::text').get().strip()
+        # pattern = re.compile(r"PEP (?P<number>\d+) – (?P<name>.+)$")
+        # match = pattern.search(pep_info)
+
+        # data = {
+        #     'number': int(match.group("number")),
+        #     'name': match.group("name"),
+        #     'status': (
+        #         response.css('dt:contains("Status") + dd abbr::text').get())
+        # }
+
+        response.meta['data']['status'] = (
+            response.css('dt:contains("Status") + dd abbr::text').get())
+        yield PepParseItem(response.meta['data'])
