fda68901402839b5518b3f13661082e3787170e0
Максим <kasyak999@yandex.ru>
2025-04-19 11:14:04 +0300

    up

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,40 +1,36 @@
-from itemadapter import ItemAdapter
 from collections import defaultdict
 import datetime
+import os
 
 
 class PepParsePipeline:
     def open_spider(self, spider):
         self.total = 0
         self.status_dict = defaultdict(int)
-        self.filename = (
-            'results/status_summary_'
-            f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
-        )
-        with open(self.filename, mode='w', encoding='utf-8') as f:
-            f.write('Статус,Количество\n')
 
-    # def open_spider(self, spider):
-    #     filename = (
-    #         'results/status_summary_'
-    #         f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
-    #     )
-    #     self.file = open(filename, mode='w', encoding='utf-8')
-    #     self.file.write('Статус,Количество\n')
+        # Получаем путь к results из FEEDS или настройки
+        feeds = spider.settings.getdict('FEEDS')
+        if feeds:
+            results_path = next(iter(feeds))
+            results_dir = os.path.dirname(results_path)
+        else:
+            results_dir = 'results'
+
+        os.makedirs(results_dir, exist_ok=True)
+        self.filename = os.path.join(
+            results_dir, (
+                f'status_summary_'
+                f'{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'
+            ))
 
     def process_item(self, item, spider):
         self.total += 1
         self.status_dict[item['status']] += 1
-
         return item
 
     def close_spider(self, spider):
-        # for status, count in self.status_dict.items():
-        #     self.file.write(f'{status},{count}\n')
-        # self.file.write(f"Total,{self.total}\n")
-        # self.file.close()
-
-        with open(self.filename, mode='a', encoding='utf-8') as f:
+        with open(self.filename, mode='w', encoding='utf-8') as f:
+            f.write('Статус,Количество\n')
             for status, count in self.status_dict.items():
                 f.write(f'{status},{count}\n')
             f.write(f"Total,{self.total}\n")
